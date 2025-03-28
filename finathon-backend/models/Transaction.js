eae3947318f2b7c0f52771fb7b9659d1f7e886e4
tejamallam <tejamallam1233@gmail.com>
2025-03-28 11:28:05 +0530

    Saving uncommitted changes before pulling

--- finathon-backend/models/Transaction.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TransactionSchema = new mongoose.Schema({
-    amount: { type: Number, required: true },
-    category: { type: String, required: true },
-    type: { type: String, enum: ["income", "expense"], required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Transaction', TransactionSchema);
